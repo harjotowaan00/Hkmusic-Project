c2c52c9091a2d612ce0cf4b7fb3fe4c3afe2c04d
harjotowaan00 <harjotlaptop00@gmail.com>
2025-11-02 00:06:50 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:4000/api/user/register", {
+      const res = await axios.post("https://spotify-project-backend-hznr.onrender.com/api/user/register", {
         name,
         email,
         password,
